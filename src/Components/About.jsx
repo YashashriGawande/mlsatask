7ee0e7be95cf3eaf75503e15f0958ec4f38b3625
Yashashri Gawande <yashashrigawande1@gmail.com>
2024-01-07 12:26:15 +0530

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an engineering student studying at MIT Academy of Engineering. I have a passion for problem-solving and innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Artificial Intelligence",
+  "Machine Learning",
+  "Web Development",
+  "Computer Vision",
+  "Internet of Things",
+  "Human Computer Interaction",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Technology excites me and I like to learn and apply it to solve real-world challenges. I have developed and refined my skills in technical analysis, project management, and collaborative teamwork.";
 
 const About = () => {
   return (
